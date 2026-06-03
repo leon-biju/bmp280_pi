2224b2836a13c65fd6967db584813da540e03671
Leon Biju <leonbiju25@gmail.com>
2026-06-03 13:23:05 +0100

    get rid of unnecessary header file

--- driver/bmp280_pi.c
+++ driver/bmp280_pi.c
@@ -2,12 +2,36 @@
 #include <linux/i2c.h>
 #include <linux/iio/iio.h>
 #include <linux/regmap.h>
-#include "bmp280_pi.h"
+#include <linux/mutex.h>
 
 #define DRIVER_NAME "bmp280_pi"
 
 #define DUMMY_TEMP_RAW      25123
 #define DUMMY_PRESSURE_RAW  101325
+
+struct bmp280_priv_state {
+        struct regmap *regmap;
+        struct mutex lock;
+
+        struct {
+                // Temperature coefficients
+                u16 dig_T1;
+                s16 dig_T2;
+                s16 dig_T3;
+
+                // Pressure coefficients omd
+                u16 dig_P1;
+                s16 dig_P2;
+                s16 dig_P3;
+                s16 dig_P4;
+                s16 dig_P5;
+                s16 dig_P6;
+                s16 dig_P7;
+                s16 dig_P8;
+                s16 dig_P9;
+
+        } parameter_buffer;
+};
 
 static const struct iio_chan_spec bmp280_pi_channels[] = {
         {
